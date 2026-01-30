c67100ecc1f10ed82b912b23c4b9072888cf0535
surafel <surafelmengist2015@gmail.com>
2026-01-30 19:39:35 +0300

    the first routes in next

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>about page! 👋</h1>
+    </main>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Home page 👋</h1>
+    </main>
+  );
+}
