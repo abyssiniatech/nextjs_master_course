a05fb966a72ef552b56db1e63daf7760b8a5e668
surafel <surafelmengist2015@gmail.com>
2026-01-31 13:57:12 +0300

    add style about product page

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,15 +7,15 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between px-6 py-4 bg-gray-900 text-white ">
    
-      <div className="flex items-center gap-3">
+      <div className="flex items-center gap-24 justify-between">
         <Image src="/logo.png" alt="Logo" width={40} height={40}  className="rounded-full"/>
         <h1 className="text-xl font-bold">My App</h1>
       </div>
       <nav className="flex gap-6">
-        <Link className="hover:bg-indigo-700 hover:px-4 py-2 rounded-md  " href="/">Home</Link>
-        <Link className="hover:bg-indigo-700 hover:px-4 py-2 rounded-md  " href="/about">About</Link>
-        <Link className="hover:bg-indigo-700 hover:px-4 py-2 rounded-md  " href="/product">product</Link>
-        <Link className="hover:bg-indigo-700 hover:px-4 py-2 rounded-md  " href="/contact">Contact</Link>
+        <Link className="hover:bg-indigo-700 hover:px-4 py-2 rounded-md active:bg-indigo-800" href="/">Home</Link>
+        <Link className="hover:bg-indigo-700 hover:px-4 py-2 rounded-md active:bg-indigo-800" href="/about">About</Link>
+        <Link className="hover:bg-indigo-700 hover:px-4 py-2 rounded-md active:bg-indigo-800" href="/product">product</Link>
+        <Link className="hover:bg-indigo-700 hover:px-4 py-2 rounded-md active:bg-indigo-800" href="/contact">Contact</Link>
       </nav>
     </header>
   );
